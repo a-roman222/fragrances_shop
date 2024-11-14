bd27b2a729f68f52f6266c65c60028872930efff
Andres <aroman222ca@gmail.com>
2024-11-13 22:08:43 -0600

    fix admin user display

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,10 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+  # Explicitly define the searchable attributes for Ransack
+  def self.ransackable_attributes(auth_object = nil)
+    # List the attributes you want to allow for searching
+    ["id", "email", "created_at", "updated_at", "remember_created_at", "reset_password_sent_at"]
+  end
 end
